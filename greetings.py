8303e7f0c23bb1559fd4e7e0371b608aafdfb5d7
flex-relax <165677991+flex-relax@users.noreply.github.com>
2024-12-23 23:53:47 +0400

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Alex"
+title = "Homework"
+content = "This is my homework"
+status = "active"
+created_date = "20-12-2024"
+issue_date = "30-12-2024"
+print("Имя пользователя: ", username)
+print("Название заметки: ", title)
+print("Содержание: ", content)
+print("Статус: ", status)
+print("Дата создания: ", created_date)
+print("Дата завершения: ", issue_date)
