f4e182875af25ac4daffdec8546124ba48f62891
flex-relax <165677991+flex-relax@users.noreply.github.com>
2025-01-11 22:32:52 +0400

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,6 +1,6 @@
 name1 = input("Введите заголовок заметки: ")
 name2 = input("Введите заголовок заметки: ")
 name3 = input("Введите заголовок заметки: ")
-list_main = (name1, name2, name3)
+list_title = (name1, name2, name3)
 
-print(list_main)
+print(list_title)
