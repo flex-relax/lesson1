5dc896e17bdef25aa6a747215a4ce5900d98e52f
flex-relax <165677991+flex-relax@users.noreply.github.com>
2025-01-14 22:06:07 +0400

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -1,2 +1,11 @@
-title = input("Введите заголовок заметки: ")
+list_title = [
+    input("Введите 1-ый заголовок заметки: "),
+    input("Введите 2-ой заголовок заметки: "),
+    input("Введите 3-ий заголовок заметки: "),
+]
 content = input("Введите текст: ")
+created_date = input("Введите дату создания заметки в формате дд.мм.гг:")
+
+print(list_title)
+print(content)
+print(created_date)
