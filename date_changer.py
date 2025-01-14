78af56e7ecf73463eb393d4537def28b0e40c77a
flex-relax <165677991+flex-relax@users.noreply.github.com>
2025-01-14 22:50:34 +0400

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,8 +1,12 @@
+#Вводим даты для заметки
 temp_created_date = input("Введите дату создания заметки в формате дд.мм.гг:")
 temp_issue_date = input("Введите дату завершения заметки в формате дд.мм.гг:")
 
+#Преобразуем даты к виду дд.мм
 created_date = temp_created_date.replace(temp_created_date[2], ".")
 issue_date = temp_issue_date.replace(temp_issue_date[2], ".")
+
+#Выводим данные
 print(
     "Дата создания заметки:",
     created_date[:5],
